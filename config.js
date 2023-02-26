f649e1c10025058af2a5ca6ade2daa7e86a99bfd
BOTZNINO <118663305+BOTZNINO@users.noreply.github.com>
2023-02-26 07:51:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.sig = 'https://instagram.com/lordagam23_'
 global.syt = 'https://youtube.com/channel/UCbbWYVvhTHz3AzfgYz4fMgg'
 global.sgh = 'https://github.com/KazeDevID/'
 global.sgc = 'https://chat.whatsapp.com/Lg1V1A0lRRA7d9Iaq117um'
-global.swa = 'wa.me/6282217590187'
+global.swa = 'wa.me/6285895921413'
 global.stg = 't.me/KenalSayaaa'
 global.swb = 'https://'
 global.snh = 'https://nhentai.net/g/365296/' //Make ini aja gausah di ganti.
@@ -45,15 +45,15 @@ global.ppulsa2 = '082217590187'
 global.psaweria = 'https://saweria.co/KazeDevID'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6282217590187'
-global.nomorown = '6282217590187'
+global.nomorbot = '6285895921413'
+global.nomorown = '6285895921413'
 global.namebot = 'KinakoBot'
-global.nameown = 'Lord Kaze'
+global.nameown = 'Lord Ninoo'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282217590187', 'Lord Kaze', 'true']
+  ['6285895921413', 'Lord Kaze', 'true']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
